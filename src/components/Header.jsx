9f3179fea703792f53d942de93f6877e14384239
Harriet Melesi <harrietmelesi@Harriets-iMac.home>
2020-06-29 16:35:16 +0100

    now committed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,31 @@ import React, { useState, useEffect } from "react";
 import { Link } from "@reach/router";
 import Navbar from "./Navbar";
 import logo from "../images/logo.png";
-import useDocumentScrollThrottled from "../hooks/scroll";
+// import useDocumentScrollThrottled from "../hooks/scroll";
 
 const Header = ({ pathname }) => {
-  const [shouldHideHeader, setShouldHideHeader] = useState(false);
-  const hiddenStyle = shouldHideHeader ? "hidden" : "";
+  // const [shouldHideHeader, setShouldHideHeader] = useState(false);
+  // const hiddenStyle = shouldHideHeader ? "hidden" : "";
 
-  const MINIMUM_SCROLL = 80;
-  const TIMEOUT_DELAY = 400;
+  // const MINIMUM_SCROLL = 80;
+  // const TIMEOUT_DELAY = 400;
 
-  useDocumentScrollThrottled((callbackData) => {
-    const { previousScrollTop, currentScrollTop } = callbackData;
-    const isScrolledDown = previousScrollTop < currentScrollTop;
-    const isMinimumScrolled = currentScrollTop > MINIMUM_SCROLL;
+  // useDocumentScrollThrottled((callbackData) => {
+  //   const { previousScrollTop, currentScrollTop } = callbackData;
 
-    setTimeout(() => {
-      setShouldHideHeader(isScrolledDown && isMinimumScrolled);
-    }, TIMEOUT_DELAY);
-  });
+  //   const isScrolledDown = previousScrollTop < currentScrollTop;
+
+  //   const isMinimumScrolled = currentScrollTop > MINIMUM_SCROLL;
+
+  //   setTimeout(() => {
+  //     setShouldHideHeader(isScrolledDown && isMinimumScrolled);
+  //   }, TIMEOUT_DELAY);
+  // });
 
   return (
-    <header className={`app-header ${hiddenStyle}`}>
+    <header className={`app-header`}>
       <Link className="link__dark" to="/">
-        <img src={logo} alt="papertrail logo" className="header__logo" />
+        <img src={logo} alt="papertrail logo" className={`header__logo`} />
       </Link>
       <Navbar pathname={pathname} />
     </header>
